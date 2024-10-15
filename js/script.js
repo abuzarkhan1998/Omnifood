87cdf282a91e9d312dd2709674467972631b9184
Abuzar Khan <abuzar.k@flatworldsolutions.com>
2024-10-16 01:20:37 +0530

    fix close mobile nav when any link is clicked

--- js/script.js
+++ js/script.js
@@ -46,6 +46,9 @@ allLinks.forEach(function (link) {
       const sectionEl = document.querySelector(href);
       sectionEl.scrollIntoView({ behavior: "smooth" });
     }
+
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
   });
 });
 
