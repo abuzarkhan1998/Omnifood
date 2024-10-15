6ccd6ce7b19289751ccb14cfdc7a33e707254495
Abuzar Khan <abuzar.k@flatworldsolutions.com>
2024-10-16 00:32:39 +0530

    Make navigation sticky using js

--- js/script.js
+++ js/script.js
@@ -48,3 +48,28 @@ allLinks.forEach(function (link) {
     }
   });
 });
+
+//Making Navigation sticky
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
