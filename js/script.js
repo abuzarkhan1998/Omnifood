432e0f3e398fec34cce23f40ebedeebab46578a0
Abuzar Khan <abuzar.k@flatworldsolutions.com>
2024-10-15 18:57:44 +0530

    Make mobile Navigation work using JS

--- js/script.js
+++ js/script.js
@@ -12,6 +12,14 @@ console.log(h1El);
 //   h1El.style.padding = "8rem";
 // });
 
+//Make Copyright year dynamic
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
+
+//Make Mobile Navigation work
+const NavbtnEl = document.querySelector(".mobile-nav");
+const headerEl = document.querySelector(".main-header");
+NavbtnEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
