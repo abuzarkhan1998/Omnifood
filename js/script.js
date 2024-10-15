8c86477b6c792c4c6ef63c7b0647fee95e21e9bc
Abuzar Khan <abuzar.k@flatworldsolutions.com>
2024-10-15 21:22:17 +0530

    Implement smooth scrolling using js

--- js/script.js
+++ js/script.js
@@ -23,3 +23,28 @@ const headerEl = document.querySelector(".main-header");
 NavbtnEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+//Implementing Smooth scrolling
+const allLinks = document.querySelectorAll("a:link");
+console.log(allLinks);
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    console.log(e);
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    console.log(href);
+
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
